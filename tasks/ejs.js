9a35e7456a37d94a907088af7ad5318130ae4c72
Matt Blair <matt@mattblair.co>
2015-07-25 22:00:39 -0700

    update to correct version of ejs-mate

--- tasks/ejs.js
+++ tasks/ejs.js
@@ -1,4 +1,4 @@
-var renderFile = require('ejs-mate');
+var ejsMate = require('ejs-mate');
 
 module.exports = function(grunt) {
   'use strict';
@@ -16,16 +16,13 @@ module.exports = function(grunt) {
       // prevents options declared / overrided
       // on file level to be moved to the next file
       options = this.options();
-      // var commonJs = options.commonJs ? 'module.exports = ':'';
-      // var out = file.src.map(grunt.file.read).join('');
+      var commonJs = options.commonJs ? 'module.exports = ':'';
       options.filename = file.src[0];
 
-      renderFile(file.src[0], options, function(err, result) {
-        if (err) {
-          done(new Error('whoops'));
-        }
+      ejsMate.compile(file.src[0], options, function(err, result) {
+        if (err) { done(err); }
 
-        grunt.file.write(file.dest, result);
+        grunt.file.write(file.dest, commonJs + result);
         grunt.log.ok('Wrote ' + file.dest);
         filesProcessed++;
 
